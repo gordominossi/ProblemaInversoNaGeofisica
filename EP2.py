98eeb99fd2572e76eb8b692445f4d8b70dba5ba6
“Bruno <bruno.outsmart@gmail.com>
2020-04-10 13:29:56 -0300

    comments

--- EP2.py
+++ EP2.py
@@ -20,49 +20,100 @@ def plot(raizes, formatação, nome, coeficientes):
 
 # Ex1
 def Ex1():
+#   function f(t, x)
+    def f(t, x):
+#       f constants
+        x_c = 0.7
+        pi = math.pi
+        beta2 = 100.0
+
+#       return 0 when x != x_c
+        if(abs(x - x_c) > 0.0001): return 0.0
+
+#       calculate f(t, x)
+        functionF = 1000.0 * c2 * (1.0 - (2.0 * beta2 * (t**2) * (pi**2))) * math.exp(-1 * beta2 * (t**2) * (pi**2))
+
+#       if f = nan | inf, returns 0.0, other case return calculated value
+        if np.isnan(functionF) or np.isinf(abs(functionF)): return 0.0
+        return functionF
+
+#   function uj_iplus1
     def uj_iplus1(uj_i, uj_iminus1, ujplus1_i, ujminus1_i, alpha2, deltaT2, fj_i):
+#       try calculate uj_iplus1, in case of error, print( Error ${f(fj_i)} )
+        print('uj_i', uj_i)
+        print('uj_iminus1', uj_iminus1)
+        print('ujplus1_i', ujplus1_i)
+        print('ujminus1_i', ujminus1_i)
+        print('alpha2', alpha2)
+        print('deltaT2', deltaT2)
+        print('fj_i', fj_i)
+
         try:
-            return -uj_iminus1 + 2.0 * (1.0 - alpha2) * uj_i + alpha2 * (ujplus1_i + ujminus1_i) + deltaT2 * fj_i
+            return -1 * uj_iminus1 + (2.0 * (1.0 - alpha2) * uj_i) + (alpha2 * (ujplus1_i + ujminus1_i)) + (deltaT2 * fj_i)
         except:
-            print(fj_i)
-    
+            print('Error', fj_i)
+
     def calculaU(T, n_t, n_x, c2, f):
+#       u calculable variables
         deltaT = T / n_t
+        deltaT2 = T * T / n_t * n_t
         deltaX = 1.0 / n_x
-        alpha2 = c2 / (deltaT / deltaX * deltaT / deltaX)
+        alpha2 = c2 / ((deltaT * deltaT) / (deltaX * deltaX))
 
-        U = np.empty((n_t + 1, n_x + 1))
+#       lenght of t and x dimensions
+        lengthT = n_t + 1
+        lengthX = n_x + 1
 
-        for j in range(n_x + 1):
+#       defines U[i][j]
+        U = np.empty((lengthT, lengthX))
+
+#       u[0 | 1][j] = 0.0
+        for j in range(1, lengthX - 1):
             U[0][j] = 0.0
             U[1][j] = 0.0
-        
-        for i in range(1, n_t):
+
+#       runs t by i and calculates t_i
+        for i in range(1, lengthT - 1):
             t_i = i * deltaT
-            U[i + 1][0] = 0.0
-            for j in range(1, n_x):
+
+#           runs x by j, and calculates x_j
+            for j in range(1, lengthX - 1):
                 x_j = j * deltaX
+
+#               calculates f(t, x)
                 fj_i = f(t_i, x_j)
-                if(fj_i > 0.00001): print(fj_i)
-                U[i + 1][j] = uj_iplus1(U[i][j], U[i-1][j], U[i][j+1], U[i][j-1], alpha2, deltaT * deltaT, fj_i)
+
+#               calculates U[i+1][j]
+                U[i + 1][j] = uj_iplus1(U[i][j], U[i-1][j], U[i][j+1], U[i][j-1], alpha2, deltaT2, fj_i)
+#                 print('U[I+1][J]', U[i + 1][j])
+
+#           fixed 0 when x = 0 or x = n_x
+            U[i + 1][0] = 0.0
             U[i + 1][n_x] = 0.0
         
         return U
 
+#   U constants
     T = 1
-
+    n_x = 10 # 1/n_x = 0.01
+    n_t = 10
     c2 = 10.0
-    n_x = int(1 / 0.1)
 
-    x_c = 0.7
-    beta = 10.0
-    def f(t, x):
-        if(abs(x - x_c) > 0.0001): return 0.0
-        ret = 1000.0 * c2 * (1.0 - 2.0 * beta * beta * t * t * math.pi * math.pi) * math.exp(-1 * beta * beta * t * t * math.pi * math.pi)
-        if np.isnan(ret) or np.isinf(abs(ret)): return 0.0
-        return ret
-    
-    n_t = 100
     U = calculaU(T, n_t, n_x, c2, f)
+
+    deltaT = T / n_t
+    deltaX = 0.1 / n_x
+
+    for i in range(1, n_t):
+        t_i = i * deltaT
+
+        for j in range(1, n_x):
+            x_j = j * deltaX
+#             print('t_i', t_i)
+#             print('x_j', x_j)
+#             print('U[t_i][x_j]', U[i+1][j+1])
+
+
+
 
 Ex1()
